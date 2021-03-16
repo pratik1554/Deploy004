9315d84689f0de005f88233d348cf71f46021b3c
pratik1554 <41234432+pratik1554@users.noreply.github.com>
2021-03-16 00:42:15 -0700

    Update proj02.2.py

--- proj02.2.py
+++ proj02.2.py
@@ -1,8 +1,10 @@
+
+!pip install matplotlib
 import streamlit as st 
+
 import numpy as np 
 
 import matplotlib.pyplot as plt
-!pip install matplotlib
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
